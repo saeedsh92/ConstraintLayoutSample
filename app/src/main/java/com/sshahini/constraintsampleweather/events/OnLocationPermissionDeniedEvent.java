258b9b382c7a2b3fe754312f156af2759fcc6b4b
saeedsh <saeedshahiniit@gmail.com>
2016-10-04 11:17:02 +0330

    Fix bug - Request location permission

---
+++ app/src/main/java/com/sshahini/constraintsampleweather/events/OnLocationPermissionDeniedEvent.java
@@ -0,0 +1,10 @@
+package com.sshahini.constraintsampleweather.events;
+
+/**
+ * @author S.Shahini
+ * @since 10/4/16
+ */
+
+public class OnLocationPermissionDeniedEvent {
+    public static final String MESSAGE="You don't permitted access to location manager";
+}
